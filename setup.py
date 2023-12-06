98a7554ad581a8c1c4af311b7694a064bab71d65
kun.xue <kun.xue@gmail.com>
2023-12-06 20:34:53 +0800

    v0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='pyxt',
-    version='0.1.1',
+    version='0.2.1',
     description='Python3 XT.COM HTTP API Connector',
     long_description=long_description,
     long_description_content_type="text/markdown",
