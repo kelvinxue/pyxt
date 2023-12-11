eaf345241f96ea7c8b964c3b457849230ebc2dd6
kun.xue <kun.xue@gmail.com>
2023-12-11 16:34:58 +0800

    v0.4.2

--- examples/websocket/perp_mark_price.py
+++ examples/websocket/perp_mark_price.py
@@ -1,20 +1,17 @@
 # -*- coding:utf-8 -*-
 import time
-from pyxt.websocket import logger
 from pyxt.websocket.perp import PerpWebsocketStreamClient
 
-logger.initLogger()
 
 if __name__ == '__main__':
     symbol = "btc_usdt"
 
 
     def message_handler(_, message):
-        logger.info(message)
+        print(message)
 
 
-    my_client = PerpWebsocketStreamClient(on_message=message_handler,
-                                          proxies={"http": "http://127.0.0.1:1088"})
+    my_client = PerpWebsocketStreamClient(on_message=message_handler)
 
     # Subscribe to a single symbol stream
     my_client.mark_price(symbol=symbol, action=PerpWebsocketStreamClient.ACTION_SUBSCRIBE)
@@ -22,5 +19,5 @@ if __name__ == '__main__':
     # # Unsubscribe
     my_client.mark_price(symbol=symbol, action=PerpWebsocketStreamClient.ACTION_UNSUBSCRIBE)
     time.sleep(5)
-    logger.info("closing ws connection")
+    print("closing ws connection")
     my_client.stop()
--- examples/websocket/spot_kline.py
+++ examples/websocket/spot_kline.py
@@ -1,20 +1,17 @@
 # -*- coding:utf-8 -*-
 import time
-from pyxt.websocket import logger
 from pyxt.websocket.spot import SpotWebsocketStreamClient
 
-logger.initLogger()
 
 if __name__ == '__main__':
     symbol = "btc_usdt"
 
 
     def message_handler(_, message):
-        logger.info(message)
+        print(message)
 
 
-    my_client = SpotWebsocketStreamClient(on_message=message_handler,
-                                          proxies={"http": "http://127.0.0.1:1088"})
+    my_client = SpotWebsocketStreamClient(on_message=message_handler)
 
     # Subscribe to a single symbol stream
     my_client.kline(symbol=symbol, interval="1m", action=SpotWebsocketStreamClient.ACTION_SUBSCRIBE)
@@ -24,5 +21,5 @@ if __name__ == '__main__':
         symbol=symbol, interval="1m", action=SpotWebsocketStreamClient.ACTION_UNSUBSCRIBE
     )
     time.sleep(5)
-    logger.info("closing ws connection")
+    print("closing ws connection")
     my_client.stop()
