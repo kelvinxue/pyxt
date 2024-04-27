74d73deef97634a12a86cf9cc89c6061269262ef
kx <kun.xue@gmail.com>
2024-04-27 11:19:30 +0800

    v0.6.16

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='pyxt',
-    version='0.6.15',
+    version='0.6.16',
     description='Python3 XT.COM HTTP API Connector',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kelvinxue/pyxt",
-    download_url='https://github.com/kelvinxue/pyxt/archive/refs/tags/v0.6.15.tar.gz',
+    download_url='https://github.com/kelvinxue/pyxt/archive/refs/tags/v0.6.16.tar.gz',
     license="MIT License",
     author="xt",
     author_email="xt@xt.com",
